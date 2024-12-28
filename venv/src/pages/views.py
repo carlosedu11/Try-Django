f59f60e3191ad8e49445d10c7bb4a7cdec09ad72
Carlosedu11 <miguelodoom22@hotmail.com>
2024-12-27 23:03:28 -0300

    For loop in a Template

--- venv/src/pages/views.py
+++ venv/src/pages/views.py
@@ -12,7 +12,8 @@ def contact_view(request, *args, **kwargs):
 def about_view(request, *args, **kwargs):
 	my_context = {
 		"my_context": "This is about us",
-		"my_number":123	
+		"my_number":123,
+		"my_list":[123, 1234, 12345]	
 	}
 
 	return render(request, "about.html", my_context)
